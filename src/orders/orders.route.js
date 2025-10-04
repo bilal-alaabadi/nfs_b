cbd4a07b112a51e1e6214e52101217b446e3413b
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-10-04 20:25:07 +0400

    وصف التغييرات هنا

--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -12,7 +12,7 @@ const THAWANI_API_URL = process.env.THAWANI_API_URL;
 const publish_key = "HGvTMLDssJghr9tlN9gr4DVYt0qyBy";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://nafascollectionom.com" }));
 app.use(express.json());
 
 // Create checkout session
@@ -163,8 +163,8 @@ router.post("/create-checkout-session", async (req, res) => {
       client_reference_id: nowId,
       mode: "payment",
       products: lineItems,
-      success_url: "http://localhost:5173/SuccessRedirect?client_reference_id=" + nowId,
-      cancel_url: "http://localhost:5173/cancel",
+      success_url: "https://nafascollectionom.com/SuccessRedirect?client_reference_id=" + nowId,
+      cancel_url: "https://nafascollectionom.com/cancel",
       metadata: {
         email: String(email || "غير محدد"),
         customer_name: String(customerName || ""),
